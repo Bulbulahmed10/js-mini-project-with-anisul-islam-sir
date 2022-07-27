b38f192fc86bde3d964322b2b0561d87374fdb4e
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-07-27 13:52:36 +0600

    added some style

--- guessing-game/main.js
+++ guessing-game/main.js
@@ -1,43 +1,46 @@
 // initializing some values
-let totalAttempts = 5
-let attempts = 0
-let totalWon = 0
-let totalLost = 0
+let totalAttempts = 5;
+let attempts = 0;
+let totalWon = 0;
+let totalLost = 0;
 // finding elements
 const form = document.querySelector("form");
 const cardBody = document.querySelector(".card-body");
 const guessingNumber = form.querySelector("#guessingNumber");
 const checkButton = form.querySelector("#check");
 const resultText = cardBody.querySelector(".resultText");
-const lostWonMessage = document.createElement("p")
+const lostWonMessage = document.createElement("p");
 const remainingAttempts = cardBody.querySelector(".remainingAttempts");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   attempts++;
-  if(attempts === 5) {
+  if (attempts === 5) {
     guessingNumber.disabled = true;
-    checkButton.disabled = true
-  }if(attempts < 6){
-    let guessNumber = Number(guessingNumber.value)
-  checkResult(guessNumber);
-    remainingAttempts.innerHTML = `Remaining attempts: ${totalAttempts - attempts}`
+    checkButton.disabled = true;
   }
-  guessingNumber.value = ""
+  if (attempts < 6) {
+    let guessNumber = Number(guessingNumber.value);
+    checkResult(guessNumber);
+    remainingAttempts.innerHTML = `Remaining attempts: ${
+      totalAttempts - attempts
+    }`;
+  }
+  guessingNumber.value = "";
 });
 
 function checkResult(guessingNumber) {
   const randomNumber = getRandomNumber(5);
   if (guessingNumber === randomNumber) {
     resultText.innerHTML = `You have won`;
-    totalWon++
+    totalWon++;
   } else {
     resultText.innerHTML = `You have lost; Random number was: ${randomNumber}`;
-    totalLost++
+    totalLost++;
   }
-  lostWonMessage.innerHTML = `Won: ${totalWon} , Lost:${totalLost}`
-  lostWonMessage.classList.add("large-text")
-  cardBody.appendChild(lostWonMessage)
+  lostWonMessage.innerHTML = `Won: ${totalWon} , Lost:${totalLost}`;
+  lostWonMessage.classList.add("large-text");
+  cardBody.appendChild(lostWonMessage);
 }
 
 function getRandomNumber(limit) {
